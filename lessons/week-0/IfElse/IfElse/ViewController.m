5c2233b3be3d14ccb2176ae333ad30a587bd764b
mikekavouras <kavourasm@gmail.com>
2015-06-05 23:47:57 -0400

    remove from this repo

--- lessons/week-0/IfElse/IfElse/ViewController.m
+++ lessons/week-0/IfElse/IfElse/ViewController.m
@@ -14,10 +14,14 @@
 
 @implementation ViewController
 
-- (IBAction)buttonTapped:(id)sender {
-    
-    NSLog(@"button tapped!");
-    
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end
