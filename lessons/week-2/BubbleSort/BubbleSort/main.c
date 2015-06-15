78586e49f0141a5e10f2fed34af998d5dde2861d
mikekavouras <kavourasm@gmail.com>
2015-06-15 13:21:44 -0400

    skeleton

--- lessons/week-2/BubbleSort/BubbleSort/main.c
+++ lessons/week-2/BubbleSort/BubbleSort/main.c
@@ -9,7 +9,73 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    // 1.
+    // name
+    // create an array of 10 ints from user input
+    
+    // 2.
+    // name
+    // store the size of the array in a variable called count
+    
+    // 3.
+    // name
+    // print array
+    
+    // 4.
+    // name
+    // add commas and spaces
+    
+    // 5.
+    // name
+    
+    // 6.
+    // name
+    // refactor so we're not using numbers[i] twice
+    
+    // 7.
+    // name
+    // print the first item and the last item
+    
+    // 8.
+    // name
+    // swap the first item and the last item
+    
+    // 8.
+    // name
+    // print array
+    
+    // 9.
+    // name
+    // swap the 1st and 2ne item if the 2nd item is larger
+    
+    // 10.
+    // name
+    // print array
+    
+    // 11.
+    // name
+    // swap the 2nd and 3rd item if the 3nd item is larger
+    
+    // 12.
+    // name
+    // print the entire array
+    
+    // 13.
+    // name
+    // bubble sort!
+    
+    // 14.
+    // name
+    // print array
+    
+    // 15.
+    // name
+    // optimization
+    
+    // 16.
+    // name
+    // print array
+    
     return 0;
 }
